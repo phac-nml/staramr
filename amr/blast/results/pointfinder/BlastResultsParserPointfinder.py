38c3cd6fa6166b9e823b1a2113d33433017e8dc7
Aaron Petkau <aaron.petkau@canada.ca>
2018-02-16 17:47:00 -0600

    Bit of refactoring/method overloading

--- amr/blast/results/pointfinder/BlastResultsParserPointfinder.py
+++ amr/blast/results/pointfinder/BlastResultsParserPointfinder.py
@@ -20,30 +20,30 @@ class BlastResultsParserPointfinder(BlastResultsParser):
                                          '%IDENTITY', '%OVERLAP', 'DB_SEQ_LENGTH/QUERY_HSP'))
 
     def _append_results_to(self, hit, results):
-        nucleotide_mutation_positions = hit.get_database_nucleotide_mutation_positions()
-        codon_mutation_positions = hit.get_database_codon_mutation_positions_at(nucleotide_mutation_positions)
+        database_nucleotide_mutation_positions = hit.get_database_nucleotide_mutation_positions()
+        database_codon_mutation_positions = hit.get_codon_mutation_positions_at(database_nucleotide_mutation_positions)
         gene=hit.get_gene()
 
-        resistance_codon_mutation_positions = self._blast_database.get_resistance_codon_mutation_positions(gene, codon_mutation_positions)
-        resistance_codons_reference_extract = hit.get_database_nucleotide_codons_at(nucleotide_mutation_positions)
-        resistance_codons_reference = self._blast_database.get_resistance_codons(gene, codon_mutation_positions)
+        database_resistance_codon_mutation_positions = self._blast_database.get_resistance_codon_mutation_positions(gene, database_codon_mutation_positions)
+        database_resistance_codons_reference = hit.get_database_nucleotide_codons_at(database_nucleotide_mutation_positions)
+        database_resistance_codons = self._blast_database.get_resistance_codons(gene, database_codon_mutation_positions)
 
-        logger.info("resistance_codons_reference_extract="+str(resistance_codons_reference_extract))
-        logger.info("resistance_codons_reference="+str(resistance_codons_reference))
+        logger.info("database_resistance_codons_reference="+str(database_resistance_codons_reference))
+        logger.info("database_resistance_codons="+str(database_resistance_codons))
 
-        if (resistance_codons_reference_extract != resistance_codons_reference):
+        if (database_resistance_codons_reference != database_resistance_codons):
             raise Exception("Error, did not extract identical codons from both blast database file and resfinder info file. blast="+
-                            str(resistance_codons_reference_extract) + ", resfinder info="+str(resistance_codons_reference))
-        elif len(resistance_codon_mutation_positions) == 0:
+                            str(database_resistance_codons_reference) + ", resfinder info="+str(database_resistance_codons))
+        elif len(database_resistance_codon_mutation_positions) == 0:
             logger.debug("No mutations for ["+hit.get_hit_id()+"]")
-        elif len(resistance_codon_mutation_positions) == 1:
+        elif len(database_resistance_codon_mutation_positions) == 1:
             results.append([hit.get_file(),
                             hit.get_hit_id(),
-                            resistance_codon_mutation_positions[0],
-                            resistance_codons_reference[0],
+                            database_resistance_codon_mutation_positions[0],
+                            database_resistance_codons[0],
                             hit.get_pid(),
                             hit.get_plength(),
                             str(hit.get_hsp_alignment_length()) + "/" + str(hit.get_alignment_length())
                             ])
         else:
-            raise Exception("Error, multiple resistance mutations for ["+hit.get_hit_id()+"], mutations "+str(resistance_codon_mutation_positions))
+            raise Exception("Error, multiple resistance mutations for ["+hit.get_hit_id()+"], mutations "+str(database_resistance_codon_mutation_positions))
--- amr/blast/results/pointfinder/PointfinderHitHSP.py
+++ amr/blast/results/pointfinder/PointfinderHitHSP.py
@@ -9,44 +9,58 @@ logger = logging.getLogger('PointfinderHitHSP')
 
 class PointfinderHitHSP(AMRHitHSP):
 
-    pp = pprint.PrettyPrinter(indent=4)
-
     def __init__(self, file, blast_record, hit, hsp):
         super().__init__(file, blast_record, hit, hsp)
 
     def get_gene(self):
         return self.hit.hit_id
 
-    def get_database_frame(self):
-        frame = self.hsp.frame[1]
+    def _get_hsp_frame(self, index):
+        frame = self.hsp.frame[index]
         if frame not in [1, -1]:
             raise Exception("frame=" + str(frame) + ", is unexpected")
         else:
             return frame
 
-    def get_database_nucleotide_mutation_positions(self):
-        start = self.hsp.sbjct_start
+    def get_database_frame(self):
+        return self._get_hsp_frame(1)
+
+    def get_query_frame(self):
+        return self._get_hsp_frame(0)
+
+    def _get_nucleotide_mutation_positions(self, start, frame):
         mutations = []
-        if self.get_database_frame() == 1:
+        if frame == 1:
             mutations = [start + i for i, c in enumerate(self.hsp.match) if c == ' ']
         else:
             mutations = [start - i for i, c in enumerate(self.hsp.match) if c == ' ']
-        logger.info("")
-        logger.info("gene: "+self.hit.title)
-        logger.info("sbjct_start: " + str(self.hsp.sbjct_start))
-        logger.info("sbjct_start: " + str(self.hsp.sbjct_end))
-        logger.info("nuc_mutations: "+str(mutations))
         return mutations
 
-    def get_database_codon_mutation_positions_at(self, nucleotide_mutation_positions):
+    def get_database_nucleotide_mutation_positions(self):
+        start = self.hsp.sbjct_start
+        frame = self.get_database_frame()
+        return self._get_nucleotide_mutation_positions(start,frame)
+
+    def get_query_nucleotide_mutation_positions(self):
+        start = self.hsp.query_start
+        frame = self.get_query_frame()
+        return self._get_nucleotide_mutation_positions(start,frame)
+
+    def get_codon_mutation_positions_at(self, nucleotide_mutation_positions):
         codon_mutation_positions = [math.ceil(x/3) for x in nucleotide_mutation_positions]
         logger.info("codon_mutations: "+str(codon_mutation_positions))
         return codon_mutation_positions
 
-    def get_database_nucleotide_codons_at(self, nucleotide_mutation_positions):
+    def _get_nucleotide_codons_at(self, nucleotide_string, nucleotide_mutation_positions):
         nucleotide_string = self.hsp.sbjct
         codon_position_starts_index_0 = [(x - 1) - ((x - 1) % 3) for x in nucleotide_mutation_positions]
         return [nucleotide_string[x:(x+3)].upper() for x in codon_position_starts_index_0]
+
+    def get_database_nucleotide_codons_at(self, nucleotide_mutation_positions):
+        return self._get_nucleotide_codons_at(self.hsp.sbjct, nucleotide_mutation_positions)
+
+    def get_query_nucleotide_codons_at(self, nucleotide_mutation_positions):
+        return self._get_nucleotide_codons_at(self.hsp.query, nucleotide_mutation_positions)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
