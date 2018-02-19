29258a4af52aa63f7510a326a8eb675bc74fde88
Aaron Petkau <aaron.petkau@canada.ca>
2018-02-19 14:12:21 -0600

    Added additional resfinder tests

--- tests/amr/AMRDetectionIT.py
+++ tests/amr/AMRDetectionIT.py
@@ -21,16 +21,47 @@ class AMRDetectionIT(unittest.TestCase):
 
         self.test_data_dir = path.join("tests", "data")
 
-    def testResfinderBetaLactam(self):
-        files = [path.join(self.test_data_dir, "beta-lactam-blaIMP-42.fsa")]
-        self.amr_detection.run_amr_detection(files, 98, 90)
+
+    def testResfinderBetaLactam2MutationsSuccess(self):
+        files = [path.join(self.test_data_dir, "beta-lactam-blaIMP-42-mut-2.fsa")]
+        self.amr_detection.run_amr_detection(files, 99, 90)
 
         resfinder_results = self.amr_detection.get_resfinder_results()
         self.assertEqual(len(resfinder_results.index), 1, 'Wrong number of rows in result')
 
-        result = resfinder_results[resfinder_results['GENE'] == 'blaIMP-42']['RESFINDER_PHENOTYPE']
-        self.assertEqual(result.size, 1, 'Wrong number of results detected')
-        self.assertEqual(result.iloc[0], 'Beta-lactam resistance', 'Wrong phenotype')
+        result = resfinder_results[resfinder_results['GENE'] == 'blaIMP-42']
+        self.assertEqual(len(result.index), 1, 'Wrong number of results detected')
+        self.assertEqual(result['RESFINDER_PHENOTYPE'].iloc[0], 'Beta-lactam resistance', 'Wrong phenotype')
+        self.assertAlmostEqual(result['%IDENTITY'].iloc[0], 99.73, places=2, msg='Wrong pid')
+
+
+    def testResfinderBetaLactam2MutationsFail(self):
+        files = [path.join(self.test_data_dir, "beta-lactam-blaIMP-42-mut-2.fsa")]
+        self.amr_detection.run_amr_detection(files, 99.8, 90)
+
+        resfinder_results = self.amr_detection.get_resfinder_results()
+        self.assertEqual(len(resfinder_results.index), 0, 'Wrong number of rows in result')
+
+
+    def testResfinderBetaLactamDelStartSuccess(self):
+        files = [path.join(self.test_data_dir, "beta-lactam-blaIMP-42-del-start.fsa")]
+        self.amr_detection.run_amr_detection(files, 99, 91)
+
+        resfinder_results = self.amr_detection.get_resfinder_results()
+        self.assertEqual(len(resfinder_results.index), 1, 'Wrong number of rows in result')
+
+        result = resfinder_results[resfinder_results['GENE'] == 'blaIMP-42']
+        self.assertEqual(len(result.index), 1, 'Wrong number of results detected')
+        self.assertEqual(result['RESFINDER_PHENOTYPE'].iloc[0], 'Beta-lactam resistance', 'Wrong phenotype')
+        self.assertAlmostEqual(result['%IDENTITY'].iloc[0], 100.00, places=2, msg='Wrong pid')
+
+
+    def testResfinderBetaLactamDelStartFail(self):
+        files = [path.join(self.test_data_dir, "beta-lactam-blaIMP-42-del-start.fsa")]
+        self.amr_detection.run_amr_detection(files, 99, 92)
+
+        resfinder_results = self.amr_detection.get_resfinder_results()
+        self.assertEqual(len(resfinder_results.index), 0, 'Wrong number of rows in result')
 
 if __name__ == '__main__':
     unittest.main()
