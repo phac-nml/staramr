f6023603abbc0959999304a404d415fba833e6f3
Aaron Petkau <aaron.petkau@canada.ca>
2018-02-21 16:09:45 -0600

    pointfinder summary

--- amr/results/AMRDetectionSummary.py
+++ amr/results/AMRDetectionSummary.py
@@ -1,3 +1,5 @@
+import pandas
+
 class AMRDetectionSummary:
 
     def __init__(self, resfinder_dataframe, pointfinder_dataframe=None):
@@ -9,12 +11,33 @@ class AMRDetectionSummary:
         else:
             self._has_pointfinder = False
 
-    def create_summary(self):
-        res_df = self._resfinder_dataframe
-        res_df_summary = res_df.groupby(['FILE']).aggregate(lambda x: {'GENE': "%s" % ', '.join(x['GENE']),
+    def _compile_resfinder_results(self, df):
+        df_summary = df.groupby(['FILE']).aggregate(lambda x: {'GENE': "%s" % ', '.join(x['GENE']),
                                                                        'RESFINDER_PHENOTYPE': "%s" % ', '.join(
                                                                            x['RESFINDER_PHENOTYPE'])})
-        res_df_summary = res_df_summary.iloc[:, 0:2]
-        res_df_summary['FILE'] = res_df_summary.index
+        df_summary = df_summary.iloc[:, 0:2]
+        df_summary['FILE'] = df_summary.index
 
-        return res_df_summary[['FILE', 'GENE', 'RESFINDER_PHENOTYPE']]
+        return df_summary[['FILE', 'GENE', 'RESFINDER_PHENOTYPE']]
+
+
+    def _compile_pointfinder_results(self, df):
+        df_summary = df.groupby(['FILE']).aggregate(lambda x: {'GENE': "%s" % ', '.join(x['GENE']),
+                                                                       'POINTFINDER_PHENOTYPE': "%s" % ', '.join(
+                                                                           x['POINTFINDER_PHENOTYPE'])})
+        df_summary = df_summary.iloc[:, 0:2]
+        df_summary['FILE'] = df_summary.index
+
+        return df_summary[['FILE', 'GENE', 'POINTFINDER_PHENOTYPE']]
+
+
+    def create_summary(self):
+        res_df = self._compile_resfinder_results(self._resfinder_dataframe)
+
+        if self._has_pointfinder:
+            point_df = self._compile_pointfinder_results(self._pointfinder_dataframe)
+            return pandas.concat([res_df, point_df])
+        else:
+            return res_df
+
+
--- tests/amr/results/AMRDetectionSummaryTest.py
+++ tests/amr/results/AMRDetectionSummaryTest.py
@@ -8,20 +8,19 @@ from amr.results.AMRDetectionSummary import AMRDetectionSummary
 class AMRDetectionSummaryTest(unittest.TestCase):
 
     def setUp(self):
-        columns_resfinder = ('FILE', 'GENE', 'RESFINDER_PHENOTYPE', '%IDENTITY', '%OVERLAP',
+        self.columns_resfinder = ('FILE', 'GENE', 'RESFINDER_PHENOTYPE', '%IDENTITY', '%OVERLAP',
                      'DB_SEQ_LENGTH/QUERY_HSP', 'CONTIG', 'START', 'END', 'ACCESSION')
-        columns_pointfinder = ('FILE', 'GENE', 'CODON_POSITION', 'NUCLEOTIDE', 'AMINO_ACID',
+        self.columns_pointfinder = ('FILE', 'GENE', 'POINTFINDER_PHENOTYPE', 'CODON_POSITION', 'NUCLEOTIDE', 'AMINO_ACID',
                      '%IDENTITY', '%OVERLAP', 'DB_SEQ_LENGTH/QUERY_HSP')
 
-        self.pointfinder_table = pandas.DataFrame([
-            ['file1', 'gyrA', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
-        ],
-            columns=columns_pointfinder)
+        # Resfinder tables
+        self.resfinder_table_empty = pandas.DataFrame([],
+            columns=self.columns_resfinder)
 
         self.resfinder_table1 = pandas.DataFrame([
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'blaIMP-42_1_AB753456', 1, 741, 'AB753456'],
         ],
-            columns=columns_resfinder)
+            columns=self.columns_resfinder)
 
         self.resfinder_table_mult_resistance = pandas.DataFrame([
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'blaIMP-42_1_AB753456', 1, 741,
@@ -29,7 +28,7 @@ class AMRDetectionSummaryTest(unittest.TestCase):
             ['file1', 'newGene', 'New resistance', 99.73, 100.00, '741/741', 'newGene', 1, 741,
              'AB753456'],
         ],
-            columns=columns_resfinder)
+            columns=self.columns_resfinder)
 
         self.resfinder_table_mult_gene_same_resistance = pandas.DataFrame([
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'blaIMP-42_1_AB753456', 1, 741,
@@ -37,7 +36,7 @@ class AMRDetectionSummaryTest(unittest.TestCase):
             ['file1', 'newGene', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'newGene', 1, 741,
              'AB753456'],
         ],
-            columns=columns_resfinder)
+            columns=self.columns_resfinder)
 
         self.resfinder_table_mult_same_gene_same_resistance = pandas.DataFrame([
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'blaIMP-42_1_AB753456', 1, 741,
@@ -45,7 +44,7 @@ class AMRDetectionSummaryTest(unittest.TestCase):
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'newGene', 1, 741,
              'AB753456'],
         ],
-            columns=columns_resfinder)
+            columns=self.columns_resfinder)
 
         self.resfinder_table_mult_file = pandas.DataFrame([
             ['file1', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'blaIMP-42_1_AB753456', 1, 741,
@@ -55,7 +54,20 @@ class AMRDetectionSummaryTest(unittest.TestCase):
             ['file2', 'blaIMP-42', 'Beta-lactam resistance', 99.73, 100.00, '741/741', 'newGene', 1, 741,
              'AB753456'],
         ],
-            columns=columns_resfinder)
+            columns=self.columns_resfinder)
+
+        # Pointfinder tables
+        self.pointfinder_table = pandas.DataFrame([
+            ['file1', 'gyrA', 'pfResistance', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
+        ],
+            columns=self.columns_pointfinder)
+
+        self.pointfinder_table_multiple_gene = pandas.DataFrame([
+            ['file1', 'gyrA', 'pfResistance', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
+            ['file1', 'gyrAB', 'pfResistance2', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
+        ],
+            columns=self.columns_pointfinder)
+
 
 
     def testResfinderSingleGene(self):
@@ -119,3 +131,45 @@ class AMRDetectionSummaryTest(unittest.TestCase):
         self.assertEqual('file2', summary['FILE'].iloc[1], 'File name not equal')
         self.assertEqual('blaIMP-42', summary['GENE'].iloc[1], 'Genes not equal')
         self.assertEqual('Beta-lactam resistance', summary['RESFINDER_PHENOTYPE'].iloc[1], 'Resfinder phenotype not equal')
+
+
+    def testPointfinderSingleGene(self):
+        amr_detection_summary = AMRDetectionSummary(self.resfinder_table_empty, self.pointfinder_table)
+
+        summary = amr_detection_summary.create_summary()
+
+        self.assertEqual(1, len(summary.index), 'Invalid number of rows in results')
+
+        self.assertEqual('file1', summary['FILE'].iloc[0], 'File name not equal')
+        self.assertEqual('gyrA', summary['GENE'].iloc[0], 'Genes not equal')
+        self.assertEqual('pfResistance', summary['POINTFINDER_PHENOTYPE'].iloc[0], 'Pointfinder phenotype not equal')
+
+
+    def testPointfinderSingleMultipleGene(self):
+        amr_detection_summary = AMRDetectionSummary(self.resfinder_table_empty, self.pointfinder_table_multiple_gene)
+
+        summary = amr_detection_summary.create_summary()
+
+        self.assertEqual(1, len(summary.index), 'Invalid number of rows in results')
+
+        self.assertEqual('file1', summary['FILE'].iloc[0], 'File name not equal')
+        self.assertEqual('gyrA, gyrAB', summary['GENE'].iloc[0], 'Genes not equal')
+        self.assertEqual('pfResistance, pfResistance2', summary['POINTFINDER_PHENOTYPE'].iloc[0], 'Pointfinder phenotype not equal')
+
+
+    def testPointfinderSingleMultipleGeneSame(self):
+        df = pandas.DataFrame([
+            ['file1', 'gyrA', 'pfResistance', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
+            ['file1', 'gyrA', 'pfResistance', 67, 'GCC -> CCC', 'A -> P', 99.96, 100.0, '2637/2637'],
+        ],
+            columns=self.columns_pointfinder)
+
+        amr_detection_summary = AMRDetectionSummary(self.resfinder_table_empty, df)
+
+        summary = amr_detection_summary.create_summary()
+
+        self.assertEqual(1, len(summary.index), 'Invalid number of rows in results')
+
+        self.assertEqual('file1', summary['FILE'].iloc[0], 'File name not equal')
+        self.assertEqual('gyrA, gyrA', summary['GENE'].iloc[0], 'Genes not equal')
+        self.assertEqual('pfResistance, pfResistance', summary['POINTFINDER_PHENOTYPE'].iloc[0], 'Pointfinder phenotype not equal')
