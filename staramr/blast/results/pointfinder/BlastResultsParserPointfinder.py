7ed0c693eb93a02630ea24b8d4dc529801c36569
Aaron Petkau <aaron.petkau@canada.ca>
2018-02-27 16:14:19 -0600

    Moved appending pointfinder results to separate method

--- staramr/blast/results/pointfinder/BlastResultsParserPointfinder.py
+++ staramr/blast/results/pointfinder/BlastResultsParserPointfinder.py
@@ -22,6 +22,19 @@ class BlastResultsParserPointfinder(BlastResultsParser):
                                        'AMINO_ACID', '%IDENTITY', '%OVERLAP', 'DB_SEQ_LENGTH/QUERY_HSP'))
         return df.set_index('FILE')
 
+    def _do_append(self, hit, db_codon, results):
+        results.append([hit.get_file(),
+                        hit.get_hit_id() + " (" + db_codon.get_database_amino_acid() + str(
+                            db_codon.get_codon_start()) + db_codon.get_query_amino_acid() + ")",
+                        self._blast_database.get_phenotype(hit.get_hit_id(), db_codon),
+                        db_codon.get_codon_start(),
+                        db_codon.get_database_codon() + ' -> ' + db_codon.get_query_codon(),
+                        db_codon.get_database_amino_acid() + ' -> ' + db_codon.get_query_amino_acid(),
+                        hit.get_pid(),
+                        hit.get_plength(),
+                        str(hit.get_hsp_alignment_length()) + "/" + str(hit.get_alignment_length())
+                        ])
+
     def _append_results_to(self, hit, database_name, results):
         database_nucleotide_mutations = hit.get_nucleotide_mutations()
 
@@ -48,17 +61,7 @@ class BlastResultsParserPointfinder(BlastResultsParser):
             logger.debug("No mutations for [" + hit.get_hit_id() + "]")
         elif len(database_resistance_codons) == 1:
             db_codon = database_resistance_codons[0]
-            results.append([hit.get_file(),
-                            hit.get_hit_id() + " (" + db_codon.get_database_amino_acid() + str(
-                                db_codon.get_codon_start()) + db_codon.get_query_amino_acid() + ")",
-                            self._blast_database.get_phenotype(hit.get_hit_id(), db_codon),
-                            db_codon.get_codon_start(),
-                            db_codon.get_database_codon() + ' -> ' + db_codon.get_query_codon(),
-                            db_codon.get_database_amino_acid() + ' -> ' + db_codon.get_query_amino_acid(),
-                            hit.get_pid(),
-                            hit.get_plength(),
-                            str(hit.get_hsp_alignment_length()) + "/" + str(hit.get_alignment_length())
-                            ])
+            self._do_append(hit, db_codon, results)
         else:
             raise Exception("Error, multiple resistance mutations for [" + hit.get_hit_id() + "], mutations " + str(
                 database_resistance_codons))
