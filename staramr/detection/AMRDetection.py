a1cbb415374268b62fc65c09209ff455cb150ad4
Aaron Petkau <aaron.petkau@canada.ca>
2018-02-27 14:23:59 -0600

    Move parsing results for resfinder/pointfinder to separate methods

--- staramr/detection/AMRDetection.py
+++ staramr/detection/AMRDetection.py
@@ -5,7 +5,8 @@ from staramr.results.AMRDetectionSummary import AMRDetectionSummary
 
 class AMRDetection:
 
-    def __init__(self, resfinder_database, amr_detection_handler, pointfinder_database=None, include_negative_results=False):
+    def __init__(self, resfinder_database, amr_detection_handler, pointfinder_database=None,
+                 include_negative_results=False):
         self._resfinder_database = resfinder_database
         self._amr_detection_handler = amr_detection_handler
         self._pointfinder_database = pointfinder_database
@@ -20,23 +21,32 @@ class AMRDetection:
         amr_detection_summary = AMRDetectionSummary(files, resfinder_dataframe, pointfinder_dataframe)
         return amr_detection_summary.create_summary(self._include_negative_results)
 
+    def _create_resfinder_dataframe(self, resfinder_blast_map, pid_threshold, plength_threshold):
+        resfinder_parser = BlastResultsParserResfinder(resfinder_blast_map, self._resfinder_database, pid_threshold,
+                                                       plength_threshold)
+        return resfinder_parser.parse_results()
+
+    def _create_pointfinder_dataframe(self, pointfinder_blast_map, pid_threshold, plength_threshold):
+        pointfinder_parser = BlastResultsParserPointfinder(pointfinder_blast_map, self._pointfinder_database,
+                                                           pid_threshold, plength_threshold)
+        return pointfinder_parser.parse_results()
+
     def run_amr_detection(self, files, pid_threshold, plength_threshold):
         self._amr_detection_handler.run_blasts(files)
 
         resfinder_blast_map = self._amr_detection_handler.get_resfinder_outputs()
-        resfinder_parser = BlastResultsParserResfinder(resfinder_blast_map, self._resfinder_database, pid_threshold,
-                                                       plength_threshold)
-        self._resfinder_dataframe = resfinder_parser.parse_results()
+        self._resfinder_dataframe = self._create_resfinder_dataframe(resfinder_blast_map, pid_threshold,
+                                                                     plength_threshold)
 
         if self._has_pointfinder:
             pointfinder_blast_map = self._amr_detection_handler.get_pointfinder_outputs()
-            pointfinder_parser = BlastResultsParserPointfinder(pointfinder_blast_map, self._pointfinder_database,
-                                                               pid_threshold, plength_threshold)
-            self._pointfinder_dataframe = pointfinder_parser.parse_results()
+            self._pointfinder_dataframe = self._create_pointfinder_dataframe(pointfinder_blast_map, pid_threshold,
+                                                                             plength_threshold)
         else:
             self._pointfinder_dataframe = None
 
-        self._summary_dataframe = self._create_amr_summary(files, self._resfinder_dataframe, self._pointfinder_dataframe)
+        self._summary_dataframe = self._create_amr_summary(files, self._resfinder_dataframe,
+                                                           self._pointfinder_dataframe)
 
     def get_resfinder_results(self):
         return self._resfinder_dataframe
