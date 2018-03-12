fd254db3f6f7dc8c3db68f8168aa112859a529c0
Aaron Petkau <aaron.petkau@canada.ca>
2018-03-12 15:03:47 -0500

    Removed unneeded method

--- staramr/blast/resfinder/ResfinderBlastDatabase.py
+++ staramr/blast/resfinder/ResfinderBlastDatabase.py
@@ -20,11 +20,6 @@ class ResfinderBlastDatabase(AbstractBlastDatabase):
         :param database_dir: The specific ResFinder database (drug class) directory.
         """
         super().__init__(database_dir)
-        self._parse_resfinder_info(os.path.join(database_dir, "notes.txt"))
-
-    def _parse_resfinder_info(self, database_info_file):
-        self._resfinder_info = pandas.read_csv(database_info_file, sep=':', comment="#", header=None, index_col=False,
-                                               names=("gene", "phenotype"))
 
     def get_database_names(self):
         return [f[:-len(self.fasta_suffix)] for f in os.listdir(self.database_dir) if
@@ -32,20 +27,3 @@ class ResfinderBlastDatabase(AbstractBlastDatabase):
 
     def get_path(self, database_name):
         return os.path.join(self.database_dir, database_name + self.fasta_suffix)
-
-    def get_phenotype(self, gene):
-        """
-        Gets the phenotype corresponding to the passed gene name for ResFinder.
-        :param gene: The gene name.
-        :return: The corresponding phenotype from the ResFinder database.
-        """
-        table = self._resfinder_info
-        phenotype = table[table['gene'] == gene]['phenotype']
-        if phenotype.size == 0:
-            logger.warning("No phenotype matches in resfinder info for gene [" + gene + "]")
-            return 'Unknown'
-        elif phenotype.size == 1:
-            return phenotype.iloc[0]
-        else:
-            raise Exception(
-                "Invalid number of matches in resfinder info for gene [" + gene + "], got=" + str(phenotype))
