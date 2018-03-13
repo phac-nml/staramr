0df695d6b30b68e0a21295a067a409141d33f90a
Aaron Petkau <aaron.petkau@canada.ca>
2018-03-13 15:26:34 -0500

    Printing paths to debug test/install

--- run-tests.sh
+++ run-tests.sh
@@ -2,9 +2,11 @@
 cd `dirname $0`
 SCRIPT_DIR=`pwd`
 
+python -c 'import sys; print(sys.path)'
+
 if [ ! -e $SCRIPT_DIR/databases ];
 then
-	./staramr.py db build
+	python staramr.py db build
 fi
 
 python -m unittest discover
