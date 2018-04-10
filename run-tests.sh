35f11477507de594d61b7a4936887afd05af925f
Aaron Petkau <aaron.petkau@canada.ca>
2018-04-10 15:41:14 -0500

    Fixing run tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd `dirname $0`
 SCRIPT_DIR=`pwd`
+export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
 
-if [ ! -e $SCRIPT_DIR/staramr/databases/data ];
+if [ ! -e $SCRIPT_DIR/staramr/databases/data/dist ];
 then
 	python bin/staramr db build
 fi
 
-nosetests
+python $SCRIPT_DIR/setup.py test
