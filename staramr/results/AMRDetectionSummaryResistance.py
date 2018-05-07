1a769ec4cc3279f8c0528717896d4646bb6cd059
Aaron Petkau <aaron.petkau@canada.ca>
2018-05-07 10:24:01 -0500

    Reformat compile_results

--- staramr/results/AMRDetectionSummaryResistance.py
+++ staramr/results/AMRDetectionSummaryResistance.py
@@ -23,7 +23,8 @@ class AMRDetectionSummaryResistance(AMRDetectionSummary):
         super().__init__(files, resfinder_dataframe, pointfinder_dataframe)
 
     def _aggregate_gene_phenotype(self, dataframe):
-        flattened_phenotype_list = [y.strip() for x in dataframe['Predicted Phenotype'].tolist() for y in x.split(self.SEPARATOR)]
+        flattened_phenotype_list = [y.strip() for x in dataframe['Predicted Phenotype'].tolist() for y in
+                                    x.split(self.SEPARATOR)]
         uniq_phenotype = OrderedDict.fromkeys(flattened_phenotype_list)
 
         return {'Gene': "%s" % (self.SEPARATOR + ' ').join(dataframe['Gene']),
@@ -36,9 +37,17 @@ class AMRDetectionSummaryResistance(AMRDetectionSummary):
         # Used to sort by gene names, ignoring case
         df_summary['Gene.Lower'] = df['Gene'].str.lower()
 
-        df_summary = df_summary.sort_values(by=['Gene.Lower']).groupby(['Isolate ID']).aggregate(
-            self._aggregate_gene_phenotype).replace({'Predicted Phenotype': {self.BLANK: 'Sensitive'}}).replace(
-            {'Predicted Phenotype': {(self.SEPARATOR + ' ') + self.BLANK: '', self.BLANK + (self.SEPARATOR + ' '): ''}}, regex=True)
+        # Compiles the gene/phenotype results into a single entry per isolate (groupby)
+        # Removes any any blank character ('-') results in the predicted phenotype unless there is no other phenotypes
+        # in which case, replace with 'Sensitive'
+        df_summary = df_summary \
+            .sort_values(by=['Gene.Lower']) \
+            .groupby(['Isolate ID']) \
+            .aggregate(self._aggregate_gene_phenotype) \
+            .replace({'Predicted Phenotype': {self.BLANK: 'Sensitive'}}) \
+            .replace({'Predicted Phenotype': {(self.SEPARATOR + ' ') + self.BLANK: '',
+                                              self.BLANK + (self.SEPARATOR + ' '): ''}},
+                     regex=True)
         return df_summary[['Gene', 'Predicted Phenotype']]
 
     def _include_negatives(self, df):
@@ -47,6 +56,6 @@ class AMRDetectionSummaryResistance(AMRDetectionSummary):
 
         negative_names_set = names_set - result_names_set
         negative_entries = pd.DataFrame([[x, 'None', 'Sensitive'] for x in negative_names_set],
-                                            columns=('Isolate ID', 'Gene', 'Predicted Phenotype')).set_index(
+                                        columns=('Isolate ID', 'Gene', 'Predicted Phenotype')).set_index(
             'Isolate ID')
         return df.append(negative_entries)
