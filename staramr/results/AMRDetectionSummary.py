03f697bb1c79c8271fdb1cb0b7c23ca3ccf7f03c
RichardDavidsonTheGreat <rcdavids@ualberta.ca>
2019-10-08 14:36:37 -0500

    Version 0_6_5. Add Criteria based on following value N50 > 10000 Genome size 4 to 6 Mbp Number of contigs under < 1000 bp

--- staramr/results/AMRDetectionSummary.py
+++ staramr/results/AMRDetectionSummary.py
@@ -28,12 +28,163 @@ class AMRDetectionSummary:
         self._resfinder_dataframe = resfinder_dataframe
         self._plasmidfinder_dataframe = plasmidfinder_dataframe
         self._mlst_dataframe = mlst_dataframe
+        self.files=files
+        self.num_files = len(self.files)
 
         if pointfinder_dataframe is not None:
             self._has_pointfinder = True
             self._pointfinder_dataframe = pointfinder_dataframe
         else:
             self._has_pointfinder = False
+    def parse_fasta(self,filepath):
+        #This solution was taken directly from https://github.com/phac-nml/sistr_cmd and was in no way desogned specifically designed for starAMR
+        '''
+        Parse a fasta file returning a generator yielding tuples of fasta headers to sequences.
+        Note:
+            This function should give equivalent results to SeqIO from BioPython
+            .. code-block:: python
+                from Bio import SeqIO
+            # biopython to dict of header-seq
+            hseqs_bio = {r.description:str(r.seq) for r in SeqIO.parse(fasta_path, 'fasta')}
+            # this func to dict of header-seq
+            hseqs = {header:seq for header, seq in parse_fasta(fasta_path)}
+            # both methods should return the same dict
+            assert hseqs == hseqs_bio
+        Args:
+            filepath (str): Fasta file path
+        Returns:
+            generator: yields tuples of (<fasta header>, <fasta sequence>)
+        '''
+        with open(filepath, 'r') as f:
+            seqs = []
+            header = ''
+            for line in f:
+                line = line.strip()
+                if line == '':
+                    continue
+                if line[0] == '>':
+                    if header == '':
+                        header = line.replace('>','')
+                    else:
+                        yield header, ''.join(seqs)
+                        seqs = []
+                        header = line.replace('>','')
+                else:
+                    seqs.append(line)
+            yield header, ''.join(seqs)
+    
+    def _get_files_contigs_lengths(self):
+        #Goes through each file in self.files and for each file determines the length of each contig. 
+        #Returns an array where each element represents a file and is itself an array of the contig lengths 
+        files_contigs_lengths =[]
+        for filepath in self.files:
+            with open(filepath,'r') as g:
+                contig_lengths = []
+                length = 0
+                for line in g:
+                    line = line.strip()
+                    if line == '':
+                        continue
+                    if line[0] == '>':
+                        if length == 0:
+                            continue
+                        else:
+                            contig_lengths.append(length)
+                            length = 0        
+                    else:
+                        length = length + len(line)
+            contig_lengths.append(length)
+            files_contigs_lengths.append(contig_lengths)
+        return files_contigs_lengths
+
+    def _get_genome_lengths(self):
+        #Goes through each file in self.files and for each file determines the length of the genome. 
+        #Returns an array where each element is the genome length of the corresponding file
+        files_genome_lengths=[]
+        for myFile in self.files:
+            parsedFile=self.parse_fasta(myFile)
+            genome_size = sum([len(s) for h, s in parsedFile])
+            files_genome_lengths.append(genome_size)
+        return files_genome_lengths
+
+    def _get_genome_length_feedback(self,files_genome_lengths):
+        #Takes as input an array where each element is the genome length of a corresponding file. 
+        #Returns an array where each elements is the feedback of whether or not the genome length for the corresponding file is between 4 Mbp and 6 Mbp
+        lb_gsize= 4000000
+        ub_gsize = 6000000
+        feedback=[]
+        for genome_length in files_genome_lengths:
+            is_gsize_acceptable = (genome_length >= lb_gsize and genome_length <= ub_gsize)
+            if is_gsize_acceptable is True:
+                feedback.append("Passed as Genome size is "+str(genome_length/1000000)+" Mbp which is between 4 Mbp and 6 Mbp")
+            else:
+                feedback.append("Failed as Genome size is "+str(genome_length/1000000)+" Mbp which is not between 4 Mbp and 6 Mbp")
+        return feedback
+
+    def _get_N50(self,files_contigs_lengths,files_genome_lengths):
+        #Takes as input an array where each element represents a file and is itself an array where each element corresponds to the length
+        #of a corresponding contig. It also takes as input an array where each element is the genome length of a corresponding file. 
+        #Returns an array where each element is the N50 value for a corresponding file
+        #For information on what N50 is and how it is calculated see https://en.wikipedia.org/wiki/N50,_L50,_and_related_statistics
+    
+        file_index =0
+        file_N50=[]
+        while file_index < self.num_files:
+            contig_lengths = files_contigs_lengths[file_index]
+            half_length=(files_genome_lengths[file_index])/2
+            contig_lengths.sort()
+            contig_num=len(contig_lengths)
+            contig_index = 1
+            sum_of_largest_contigs=0
+            while contig_index < contig_num:
+                if sum_of_largest_contigs+contig_lengths[contig_num-contig_index] >=half_length:
+                    break
+                else: 
+                    sum_of_largest_contigs=sum_of_largest_contigs+contig_lengths[contig_num-contig_index]
+                    contig_index=contig_index+1
+            file_N50.append(contig_lengths[contig_num-contig_index])
+            file_index = file_index +1
+        return file_N50
+
+    def _get_N50_feedback(self,N50_values:[]):
+        #Takes as input an array where each element is the N50 value of a corresponding file. 
+        #Returns an array where each elements is the feedback of whether or not the N50 length for the corresponding file is over 10 000
+        N50_feedback = []
+        for file_N50_value in N50_values:
+            if file_N50_value > 10000:
+                N50_feedback.append("Passed as N50 value is "+str(file_N50_value)+" which is greater than 10000")
+            else:
+                N50_feedback.append("Failed as N50 value is "+str(file_N50_value)+" which is less than or equal to 10000")
+        return N50_feedback
+    
+    def _get_num_contigs_under_minimum_bp(self,files_contigs_lengths,minimum_contig_length):
+        #Takes as input an array where each element represents a file and is itself an array where each element corresponds to the length
+        #of a corresponding contig. It also take in as input the predefined minimum length that a contig can be without raising concern. 
+        #Returns an array where each element denotes the number of contigs below this predefined minimum contig length for the corresponding file
+        file_num_contigs=[]
+        file_index = 0
+        while file_index < self.num_files:
+            num_contigs = 0
+            for contig in files_contigs_lengths[file_index]:
+                if contig < minimum_contig_length:
+                    num_contigs = num_contigs+1
+            file_num_contigs.append(num_contigs)
+            file_index=file_index+1
+        return file_num_contigs
+    
+    def _get_num_contigs_under_minimum_bp_feedback(self,num_contigs_under_minimum_bp,minimum_contig_length,unacceptable_num_contigs_under_minimum_bp):
+        #Takes as input an array where each element denotes the number of contigs below our predefined minimum contig length for the corresponding file.
+        #It also takes as input the predefined minimum length that a contig can be without raising concern. Finally, it takes as input the minimum
+        #number of contigs which raise concern in order for us to determine that a file fails. 
+        #Returns an array where each elements is the feedback of whether or not the corresponding file has too many contigs that are smaller than our predefined 
+        #minimum contig length
+        contigs_under_minimum_bp_feedback=[]
+        for file_num_contigs_under_minimum_bp in num_contigs_under_minimum_bp:
+            if file_num_contigs_under_minimum_bp >= unacceptable_num_contigs_under_minimum_bp:
+                contigs_under_minimum_bp_feedback.append("Failed as the number of contigs less than "+str(minimum_contig_length)+" bp is "+str(file_num_contigs_under_minimum_bp)+" which is greater than or equal to "+str(unacceptable_num_contigs_under_minimum_bp))
+            else:
+                contigs_under_minimum_bp_feedback.append("Passed as the number of contigs less than "+str(minimum_contig_length)+" bp is "+str(file_num_contigs_under_minimum_bp)+" which is less than "+str(unacceptable_num_contigs_under_minimum_bp))
+        return contigs_under_minimum_bp_feedback
 
     def _compile_results(self, resistance_frame: DataFrame) -> DataFrame:
         df_summary = resistance_frame.sort_values(by=['Gene']).groupby(['Isolate ID']).aggregate(
@@ -157,6 +308,23 @@ class AMRDetectionSummary:
             mlst_merging_frame = mlst_frame[['Scheme', 'Sequence Type']]
             resistance_frame = resistance_frame.merge(mlst_merging_frame, on='Isolate ID', how='left')
 
+        name_set=[]
+        for myFile in self.files:
+            name_set.append(path.splitext(path.basename(myFile))[0])
+        files_genome_lengths = self._get_genome_lengths()
+        files_genome_length_feedback = self._get_genome_length_feedback(files_genome_lengths)
+        files_contigs_lengths=self._get_files_contigs_lengths()
+        files_N50_values=self._get_N50(files_contigs_lengths,files_genome_lengths)
+        files_N50_feedback=self._get_N50_feedback(files_N50_values)
+        minimum_contig_length=1000
+        files_contigs_under_minimum_bp= self._get_num_contigs_under_minimum_bp(files_contigs_lengths,minimum_contig_length)
+        unacceptable_num_contigs_under_minimum_bp= 1000
+        file_num_contigs_under_minimum_bp_feedback= self._get_num_contigs_under_minimum_bp_feedback(files_contigs_under_minimum_bp,minimum_contig_length,unacceptable_num_contigs_under_minimum_bp)
+
+        quality_module_frame=pd.DataFrame([[t,u,v,w,x,y,z] for t,u,v,w,x,y,z in zip(name_set,files_genome_lengths,files_genome_length_feedback,files_N50_values, files_N50_feedback,files_contigs_under_minimum_bp,file_num_contigs_under_minimum_bp_feedback)],
+            columns=('Isolate ID', 'Genome Length','Genome Length Feedback','N50 value','N50 Feedback','Number of Contigs Under '+str(minimum_contig_length)+' bp','Number of Contigs Under '+str(minimum_contig_length)+' bp Feedback')).set_index('Isolate ID')
+
+        resistance_frame = resistance_frame.merge(quality_module_frame, on='Isolate ID', how='left')
         return resistance_frame.sort_index()
 
     def _get_detailed_summary_columns(self):
