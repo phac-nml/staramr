3d6317ca7c4b7aa726fd497971170d758370c77a
Eric Marinier <emarinier@gmail.com>
2023-08-16 10:07:13 -0500

    Removing misleading comments.

--- staramr/tests/unit/results/test_ARGDrugTablePointfinder.py
+++ staramr/tests/unit/results/test_ARGDrugTablePointfinder.py
@@ -17,6 +17,4 @@ class ARGDrugTablePointfinderTest(unittest.TestCase):
         drug = self.arg_drug_table.get_drug("escherichia_coli", "parC", 57)
 
         # Specifically, we're interested in it not crashing.
-        # Depending on the version of pandas, this may return NaN or None.
-        # pandas>2 will catch None, whereas pandas<2 will not catch None.
         self.assertTrue(pandas.isna(drug))
